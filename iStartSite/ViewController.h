9123ef2def692b012073c30a808151a155cdd6f3
Szilard Antal <sziszo@sys-mbp.local>
2015-01-02 00:04:13 +0100

    Initial Commit

---
+++ iStartSite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iStartSite
+//
+//  Created by Szilard Antal on 2015. 01. 02..
+//  Copyright (c) 2015. Szilard Antal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
